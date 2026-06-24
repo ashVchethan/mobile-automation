5aa699abfea0e24badf673e872b62793ad3c187b
ashVchethan <ashwichethan92@gmail.com>
2026-06-24 17:45:17 +0530

    added soft assert

--- src/main/java/pages/login/Login.java
+++ src/main/java/pages/login/Login.java
@@ -12,11 +12,26 @@ public class Login extends PageUtils {
     @iOSXCUITFindBy(id = "inputText")
     private WebElement mobileInput;
 
+    @AndroidFindBy(id = "password")
+    @iOSXCUITFindBy(id = "password")
+    private WebElement password;
+
+    @AndroidFindBy(id = "continue")
+    @iOSXCUITFindBy(id = "continue")
+    private WebElement continueButton;
+
     public boolean login() {
         return true;
     }
 
+    public boolean isLoginPageOpened(){ return isElementVisible(mobileInput,"Navigated mobile number");}
+
     public void inputMobileNumber(String mobileNum) {
         mobileInput.sendKeys(mobileNum);
     }
+    public void clickContinue() {
+        isElementVisibleThenClick(continueButton);
+    }
+    public boolean isPasswordPageOpened(){ return isElementVisible(password,"Navigated mobile number");}
+
 }
--- src/test/java/android/LoginTest.java
+++ src/test/java/android/LoginTest.java
@@ -9,6 +9,7 @@ import global.BaseTest;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import org.testng.asserts.SoftAssert;
 import pages.login.Login;
 
 public class LoginTest extends BaseTest {
@@ -19,9 +20,16 @@ public class LoginTest extends BaseTest {
         credentials = Fixture.from(LoginCredentials.class).gimme("LoginCredentials");
     }
 
+
+   // @Test(dataProvider = "LoginCredentials",  groups = { "login" }, priority = 1,dependsOnMethods = {"add DB connect here, method name"})
     @Test
     public void verifyAndroidValidLogin() {
         Login login = new Login();
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(login.isLoginPageOpened() ,"Failed to open the LoginPage");
         login.inputMobileNumber(credentials.getMobileNumber());
+        login.clickContinue();
+        softAssert.assertTrue(login.isPasswordPageOpened() ,"Failed to open the password screen");
+        softAssert.assertAll();
     }
 }
